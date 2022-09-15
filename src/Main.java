5e09ce1466d33c476e8f924c1397afebb6b844df
Rafikova Darya <darya.sladkova.89@mail.ru>
2022-09-15 16:08:08 +0400

    add predicate

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList();
-        people.add(new Person("Даенерис", "Таргариен Бурерожденная Кхалиси Великого травяного моря", 28));
+        people.add(new Person("Даенерис", "Таргариен Бурерожденная Кхалиси Великого травяного моря", 10));
         people.add(new Person("Джон", "Аррен Лорд Орлиного Гнезда", 70));
-        people.add(new Person("Джон", "Сноу", 30));
+        people.add(new Person("Джон", "Сноу", 14));
         people.add(new Person("Иван", "Безродный", 40));
+
+        Predicate<Person> age = (person) -> {
+            return person.getAge() < 18;
+        };
+        people.removeIf(age);
         Collections.sort(people, new NumberWordsSurnameComparator(4));
         System.out.println(people);
     }
